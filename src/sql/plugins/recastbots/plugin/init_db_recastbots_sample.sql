ed6f0cc23a8e5f89da328e372b3d16974a6679fc
pierrelevy <pierrelevy@users.noreply.github.com>
2017-11-21 18:46:53 +0100

    RECASTBOTS-2 : data sample

---
+++ src/sql/plugins/recastbots/plugin/init_db_recastbots_sample.sql
@@ -0,0 +1,2 @@
+INSERT INTO recastbots_bot (id_recast_bot, bot_key, name, description, avatar_url, language, bot_status, token) VALUES
+	(1, 'lutece', 'Lutece', 'A propos de Lutece', '', 'fr', 1, '7c63de7b6afdbdc4808f52dfe4d55548');
